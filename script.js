6df18e73bf4ba7f6b46792a7235597e6cd75665b
jaa13c <jaa13c@my.fsu.edu>
2015-11-12 23:46:28 +0000

    Lesson 8 Assignment first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+/**
+* Created with LIS2360_Lesson8_jQuery.
+* User: jaa13c
+* Date: 2015-11-12
+* Time: 11:42 PM
+* To change this template use Tools | Templates.
+*/
+define(function() {
+return {};
+});
